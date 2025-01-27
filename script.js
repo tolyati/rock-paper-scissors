7f2f0efb4f8d440ba02d345a4188ef13344d134e
tolyati <tolyakretsu@gmail.com>
2025-01-27 20:24:47 +0200

    Fix buttons

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('.button').forEach(button => {
+    button.addEventListener('mouseenter', () => {
+        button.style.backgroundColor = '#888';
+    });
+
+    button.addEventListener('mouseleave', () => {
+        button.style.backgroundColor = '';
+    });
+});
