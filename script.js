8feec04b057f33066b0752d8d59934b8a607c68b
tolyati <tolyakretsu@gmail.com>
2025-02-12 23:40:49 +0200

    Playing with width

--- script.js
+++ script.js
@@ -1,16 +1,51 @@
-const movingText = document.getElementById('moving-text');
-let position = 0;
+// const movingText = document.getElementById('moving-text');
+// let position = 0;
 
-function moveText() {
-  position += 5; 
-  movingText.style.left = position + 'px'; 
+// function moveText() {
+//   position += 5; 
+//   movingText.style.left = position + 'px'; 
   
-  if (position >= window.innerWidth - 600) { 
-    position = -movingText.offsetWidth - 600; 
-  }
+//   if (position >= window.innerWidth - 600) { 
+//     position = -movingText.offsetWidth - 600; 
+//   }
+// }
+
+// setInterval(moveText, 20); 
+
+window.onresize = () => {
+  console.log(window.innerWidth);
 }
 
-setInterval(moveText, 20); 
+
+
+const movingText = document.getElementById('moving-text');
+let position = 0;
+let direction = 1; // 1 = вправо, -1 = влево
+const speed = 2;
+
+function moveText() {
+  position += speed * direction;
+            
+  if (position > window.innerWidth - 550) {
+    position = -movingText.offsetWidth - 600; // Выход слева после правого края
+  }
+  if (position < -movingText.offsetWidth + 600) {
+    position = window.innerWidth - 600; // Выход справа после левого края
+  }
+
+  movingText.style.left = position + 'px';
+  requestAnimationFrame(moveText);
+}
+
+movingText.addEventListener('click', () => {
+  direction *= -1; // Меняем направление
+});
+
+window.onload = () => {
+  movingText.style.left = "0px";
+  moveText();
+};
+
 
 function start() {
    window.open("./pages/Gameitself/Game.html", "_blank");
