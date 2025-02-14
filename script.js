96b225928130781515d07430f90d0aa0ff943960
tolyati <tolyakretsu@gmail.com>
2025-02-14 20:46:56 +0200

    Deleted some unused script

--- script.js
+++ script.js
@@ -55,38 +55,3 @@ window.onload = () => {
 function start() {
    window.open("./pages/Gameitself/Game.html", "_blank");
 }
-
-async function fetchData(){
-
-  const category = 'wildlife';
-  const apiKey = '9KsCzEYpSGRbIE3mDOu7zA==DLn1SzRPV0RqeykG';
-  const url = `https://api.api-ninjas.com/v1/randomimage?category=${category}`;
-
-  try {
-      const response = await fetch(url, {
-          method: 'GET',
-          headers: {
-              'X-Api-Key': apiKey,
-              'Accept': 'image/jpg'
-          }
-      });
-
-      if (!response.ok) {
-          throw new Error(`Error: ${response.statusText}`);
-      }
-
-      const imageBlob = await response.blob(); 
-      const imageUrl = URL.createObjectURL(imageBlob); 
-
-      const imgElement = document.getElementById("randphoto");
-      imgElement.src = imageUrl;
-      imgElement.style.display = "block";
-      // imgElement.addEventListener('load', () => {
-      //     container.innerHTML += '<button>New paragraph</button>';
-      //   });
-
-  } catch (error) {
-      console.error(error);
-  }
-
-}
